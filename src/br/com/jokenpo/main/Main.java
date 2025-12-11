7510f1a6f21cf8fb51b856d7ad23f609d4fc859a
Hedersonn <rigonatti.hederson@gmail.com>
2025-12-11 08:56:10 -0300

    Add freatures to the game

--- src/br/com/jokenpo/main/Main.java
+++ src/br/com/jokenpo/main/Main.java
@@ -6,14 +6,30 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        JokenpoUtils game = new JokenpoUtils();
-        Random randomNumber = new Random();
-        int computerChoice = randomNumber.nextInt(1, 4);
-        System.out.println("<< Rock Paper Scissors >>");
-        System.out.print("Choice your option: ");
-        Scanner scanner = new Scanner(System.in);
-        int playerChoice = scanner.nextInt();
-        game.determineWinner(playerChoice, computerChoice);
+        while (true) {
+            System.out.println("1. Rock\n2. Paper\n3.Scissors");
+
+            JokenpoUtils game = new JokenpoUtils();
+            Random randomNumber = new Random();
+            int computerChoice = randomNumber.nextInt(1, 4);
+            System.out.println("<< Rock Paper Scissors >>");
+            System.out.print("Choose your option: ");
+            Scanner scanner = new Scanner(System.in);
+            int playerChoice = scanner.nextInt();
+            game.determineWinner(playerChoice, computerChoice);
+
+            System.out.println("Press (Y) to continue or any another key to exit: ");
+            Scanner scannerContinue = new Scanner(System.in);
+            String playerContinueChoice = scannerContinue.next();
+
+
+            if(playerContinueChoice.charAt(0) == 'Y' || playerContinueChoice.charAt(0) == 'y') {
+                // continue
+            } else {
+                System.out.println("Shutting Down...");
+                break;
+            }
+        }
     }
 }
 
